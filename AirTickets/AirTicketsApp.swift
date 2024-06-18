5278209d265b314ac3b6557421b06487fd226d3e
Artem Amaev <artemamaev2@gmail.com>
2024-06-18 10:54:57 +0300

    Initial Commit

---
+++ AirTickets/AirTicketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirTicketsApp.swift
+//  AirTickets
+//
+//  Created by Артём Амаев on 18.06.24.
+//
+
+import SwiftUI
+
+@main
+struct AirTicketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
