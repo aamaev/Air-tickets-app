b96f4ea5584ee4e5ec92d73191ca81cb8dd6dc2d
Amaev Artem <64634869+aamaev@users.noreply.github.com>
2024-06-21 08:34:35 +0300

    update MenuView.swift

--- AirTickets/Views/MenuView.swift
+++ AirTickets/Views/MenuView.swift
@@ -7,16 +7,53 @@
 
 import SwiftUI
 
-class MainViewModel: ObservableObject {
-    @Published var selectedMenuItem: MenuItem = .tickets
-    
-    func selectMenuItem(_ item: MenuItem) {
-        selectedMenuItem = item
+struct MenuView: View {
+    @ObservedObject var viewModel: MenuViewModel
+
+    var body: some View {
+        TabView(selection: $viewModel.selectedMenuItem) {
+            viewModel.coordinator.showSearchFlights()
+                .tabItem {
+                    Image(.airplane).renderingMode(.template)
+                    Text("Авиабилеты")
+                        .font(.custom("SFProDisplay-Regular", size: 10))
+                }
+                .tag(MenuItem.tickets)
+            
+            viewModel.coordinator.showHotels()
+                .tabItem {
+                    Image(.hotels).renderingMode(.template)
+                    Text("Отели")
+                        .font(.custom("SFProDisplay-Regular", size: 10))
+                }
+                .tag(MenuItem.hotels)
+            
+            viewModel.coordinator.showShortcuts()
+                .tabItem {
+                    Image(.geoPoint).renderingMode(.template)
+                    Text("Короче")
+                        .font(.custom("SFProDisplay-Regular", size: 10))
+                }
+                .tag(MenuItem.shortcuts)
+            
+            viewModel.coordinator.showSubscriptions()
+                .tabItem {
+                    Image(.notifications).renderingMode(.template)
+                    Text("Подписки")
+                        .font(.custom("SFProDisplay-Regular", size: 10))
+                }
+                .tag(MenuItem.subscriptions)
+            
+            viewModel.coordinator.showProfile()
+                .tabItem {
+                    Image(.person).renderingMode(.template)
+                    Text("Профиль")
+                        .font(.custom("SFProDisplay-Regular", size: 10))
+                }
+                .tag(MenuItem.profile)
+        }
     }
 }
 
-enum MenuItem: Hashable {
-    case tickets, hotels, shortcuts, subscriptions, profile
-}
 
 
